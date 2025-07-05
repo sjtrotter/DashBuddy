07f5ddeb8ae6d064cbfddf36eda54416ce7f9c78
sjtrotter <stephen.j.trotter@gmail.com>
2025-07-05 09:41:20 -0500

    added cleanup/self-healing of active order and queue; should be empty if we are in this state.

--- app/src/main/java/cloud/trotter/dashbuddy/state/handlers/AwaitingOffer.kt
+++ app/src/main/java/cloud/trotter/dashbuddy/state/handlers/AwaitingOffer.kt
@@ -1,5 +1,6 @@
 package cloud.trotter.dashbuddy.state.handlers
 
+import cloud.trotter.dashbuddy.DashBuddyApplication
 import cloud.trotter.dashbuddy.log.Logger as Log
 import cloud.trotter.dashbuddy.state.AppState as AppState
 import cloud.trotter.dashbuddy.state.StateContext as StateContext
@@ -7,6 +8,9 @@ import cloud.trotter.dashbuddy.state.StateHandler
 import cloud.trotter.dashbuddy.services.accessibility.screen.Screen
 
 class AwaitingOffer : StateHandler {
+
+    private val tag = "AwaitingOfferHandler"
+    private val currentRepo = DashBuddyApplication.currentRepo
 
     override suspend fun processEvent(
         stateContext: StateContext,
@@ -23,7 +27,38 @@ class AwaitingOffer : StateHandler {
 
             screen.isPickup -> AppState.DASH_ACTIVE_ON_PICKUP
             screen.isDelivery -> AppState.DASH_ACTIVE_ON_DELIVERY
-            else -> currentState
+            else -> {
+                // at this point, we have been on this screen for two separate events:
+                // -- the enterState event
+                // -- and here in processEvent (at least once)
+                // in this state, there should be NO active orders.
+                // so let's clear the queue and active order, if there are any.
+                try {
+                    if (stateContext.currentDashState?.activeOrderId != null) {
+                        Log.d(
+                            tag,
+                            "Active order found in state: ${stateContext.currentDashState.activeOrderId} - clearing."
+                        )
+                        currentRepo.setActiveOrderId(null)
+                    }
+                    if (stateContext.currentDashState?.activeOrderQueue?.isNotEmpty() == true) {
+                        Log.d(
+                            tag,
+                            "Active order queue found in state: ${stateContext.currentDashState.activeOrderQueue} - clearing."
+                        )
+                        for (order in stateContext.currentDashState.activeOrderQueue) {
+                            currentRepo.removeOrderFromQueue(order)
+                        }
+                    }
+                } catch (e: Exception) {
+                    Log.e(
+                        tag,
+                        "!!! Error clearing queue !!! - check logs!",
+                        e
+                    )
+                }
+                currentState
+            }
         }
     }
 
